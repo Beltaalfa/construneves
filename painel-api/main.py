7dac20291312e0fa6e4aa5a77ddf96d572d40678
PMG Infra Backup <infra@pmg.local>
2026-04-13 12:44:27 +0000

    backup: snapshot completo 2026-04-13T12:44Z

--- painel-api/main.py
+++ painel-api/main.py
@@ -29,7 +29,12 @@ PASSWORD = _env("FIREBIRD_PASSWORD", "")
 app = FastAPI(title="Construneves Firebird API", version="1.0.0")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:3000", "http://localhost:3000"],
+    allow_origins=[
+        "http://127.0.0.1:3000",
+        "http://localhost:3000",
+        "http://127.0.0.1:3001",
+        "http://localhost:3001",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
